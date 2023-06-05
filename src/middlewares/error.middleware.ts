6687daa9d1c604ca184c5848e21beace191c95ee
Matin Askari <dev.mtnaskari@gmail.com>
2023-06-06 01:20:03 +0330

    fix(middleware): Fix Invalid json body error

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,9 +1,13 @@
 import { NextFunction, Request, Response } from 'express'
 
-import { httpResponse } from '../utils/http-response'
+import { HttpResponse } from '../utils/http-response'
 
 export function errorMiddleware(error: any, req: Request, res: Response, next: NextFunction) {
-  httpResponse.internalServerError(res)
+  if (error instanceof SyntaxError && 'body' in error) {
+    return HttpResponse.badRequest(res, ['Invalid json body'])
+  }
+
+  HttpResponse.internalServerError(res)
 
   next(error)
 }

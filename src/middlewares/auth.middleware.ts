d3c94ffa0dfbee48b23e9a887d4ba237f8c3d019
Matin Askari <dev.mtnaskari@gmail.com>
2023-06-06 01:24:28 +0330

    feature(middlewares): Add auth protect middleware

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from 'express'
+
+import { JWT_SECRET } from '../config/env.config'
+import { UserModel } from '../database/models/user.model'
+import { jwtVerifyPromisified } from '../services/auth.service'
+import { HttpResponse } from '../utils/http-response'
+
+export async function protect(req: Request, res: Response, next: NextFunction) {
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer')) {
+    return HttpResponse.unauthorized(res, ['You are not logged in'])
+  }
+
+  const token = req.headers.authorization.split(' ')[1]
+
+  const decoded = await jwtVerifyPromisified(token, JWT_SECRET)
+
+  const user = await UserModel.findById(decoded.id)
+
+  res.locals.user = user
+
+  next()
+}

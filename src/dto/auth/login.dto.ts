cb43bbd1dd61b6a4773f5c2a9b43ed3826d4191d
Matin Askari <dev.mtnaskari@gmail.com>
2023-06-06 01:18:11 +0330

    feature(dto): Add login dto

---
+++ src/dto/auth/login.dto.ts
@@ -0,0 +1,11 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator'
+
+export class LoginDTO {
+  @IsNotEmpty()
+  @IsEmail()
+  email: string
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string
+}

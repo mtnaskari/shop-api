3ba36c882c04029a8ccc20f6e5606de304582faf
Matin Askari <dev.mtnaskari@gmail.com>
2023-06-06 01:18:40 +0330

    feature(dto): Add register dto

---
+++ src/dto/auth/register.dto.ts
@@ -0,0 +1,14 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator'
+
+export class RegisterDTO {
+  @IsNotEmpty()
+  fullName: string
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string
+
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string
+}

362f17b55a7a8eb787478a1cd79444d3e0fd1141
Matin Askari <dev.mtnaskari@gmail.com>
2023-06-06 01:20:42 +0330

    feature(main): Add auth controller to main function

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
-import { ProductController } from './controllers/product.controller'
+import { AuthController } from './controllers/auth.controller'
 import { connectToDatabase } from './database/db.connection'
 import { Server } from './server/server'
 
 async function main(): Promise<void> {
-  const server = new Server([new ProductController()])
+  const server = new Server([new AuthController()])
   server.listen()
   await connectToDatabase()
 }

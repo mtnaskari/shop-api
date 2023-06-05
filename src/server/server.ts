181d7304605ad5708bec9c84984ef0dccb648221
Matin Askari <dev.mtnaskari@gmail.com>
2023-06-05 11:36:04 +0330

    feature(server): Create express server class

---
+++ src/server/server.ts
@@ -0,0 +1,47 @@
+import express, { Application } from 'express'
+import morgan from 'morgan'
+
+import { SERVICE_HOST, SERVICE_PORT } from '../config/env.config'
+import { Controller } from '../controllers/controller.interface'
+import { errorMiddleware } from '../middlewares/error.middleware'
+import { httpResponse } from '../utils/http-response'
+
+export class Server {
+  public app: Application
+
+  constructor(controllers: Controller[]) {
+    this.app = express()
+    this.initializeMiddlewares()
+    this.initializeControllers(controllers)
+    this.notFoundHandler()
+    this.errorHandler()
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(morgan('dev'))
+    this.app.use(express.json())
+    this.app.use(express.urlencoded({ extended: true }))
+  }
+
+  private initializeControllers(controllers: Controller[]) {
+    controllers.forEach((controller) => {
+      this.app.use('/', controller.router)
+    })
+  }
+
+  private notFoundHandler() {
+    this.app.use((_req, res) => {
+      return httpResponse.notFound(res)
+    })
+  }
+
+  private errorHandler() {
+    this.app.use(errorMiddleware)
+  }
+
+  public listen() {
+    this.app.listen(SERVICE_PORT, SERVICE_HOST, () => {
+      console.log(`[i]: Server is running at http://${SERVICE_HOST}:${SERVICE_PORT} 🚀`)
+    })
+  }
+}

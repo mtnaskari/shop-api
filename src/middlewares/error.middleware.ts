7c8b9d90d547cc3080edc832a398b6fdfb31c333
Matin Askari <dev.mtnaskari@gmail.com>
2023-06-10 03:06:05 +0330

    feature(midleare): Handle http exception in error middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,11 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
 
+import { HttpException } from '../utils/http-exception'
 import { HttpResponse } from '../utils/http-response'
 
 export function errorMiddleware(error: any, req: Request, res: Response, next: NextFunction) {
   if (error instanceof SyntaxError && 'body' in error) {
     return HttpResponse.badRequest(res, ['Invalid json body'])
   }
+
+  if (error instanceof HttpException) HttpResponse.error(res, error.statusCode, [error.message])
 
   HttpResponse.internalServerError(res)
 

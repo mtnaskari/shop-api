42e32f3468547055c8e3520a7ac935672c3f3807
Matin Askari <dev.mtnaskari@gmail.com>
2023-06-11 00:47:37 +0330

    feature(middlewares): Add restrict method to auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -3,6 +3,7 @@ import { injectable } from 'tsyringe'
 
 import { JWT_SECRET } from '../config/env.config'
 import { UserModel } from '../database/models/user.model'
+import { UserRole } from '../interfaces/user.interface'
 import { AuthService } from '../services/auth.service'
 import { HttpResponse } from '../utils/http-response'
 
@@ -10,13 +11,13 @@ import { HttpResponse } from '../utils/http-response'
 export class AuthMiddleware {
   constructor(private readonly authService: AuthService) {}
 
-/**
- * Protect a route from being accessed by unauthenticated users
- * @param req The request
- * @param res The response
- * @param next The next function 
- * @returns void 
- */
+  /**
+   * Protect a route from being accessed by unauthenticated users
+   * @param req The request
+   * @param res The response
+   * @param next The next function
+   * @returns void
+   */
   public protect = async (req: Request, res: Response, next: NextFunction) => {
     if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer')) {
       return HttpResponse.unauthorized(res, ['You are not logged in'])
@@ -30,5 +31,20 @@ export class AuthMiddleware {
     res.locals.user = user
 
     next()
+  }
+
+  /**
+   * Restrict access to the specified user roles
+   * @param roles The user roles allowed to access the route
+   * @returns Middleware function
+   */
+  public restrictTo = (...roles: UserRole[]) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+      const user = res.locals.user
+
+      return !user || !roles.includes(user.role)
+        ? HttpResponse.forbidden(res, ['You are not authorized to perform this action'])
+        : next()
+    }
   }
 }
